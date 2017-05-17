783a7281a5679b09483218097583d0bf7fc39106
Brendon Otto <brendon@brendonotto.com>
2017-05-17 06:48:19 -0500

    Including source

---
+++ lib/board.ex
@@ -0,0 +1,24 @@
+defmodule IslandsEngine.Board do
+    alias IslandsEngine.Coordinate
+
+    def start_link do
+        Agent.start_link(fn -> %{} end)
+    end
+    
+@letters ~W(a b c d e f g h i j)
+@numbers [1,2,3,4,5,6,7,8,9,10]
+
+    defp keys() do
+        for letter <- @letters, number <- @numbers do
+            String.to_atom("#{letter}#{number}")
+        end
+    end
+
+    defp initialize_board() do
+        Enum.reduce(keys(), %{}, fn(key, board) ->
+            {:ok, coord} = Coordinate.start_link
+            Map.put_new(board, key, coord)
+        end )
+    end
+    
+end
